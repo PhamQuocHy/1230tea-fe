48bc7f5d9b8af0030ea612834d4373ae2284d9f7
tranhoangkhoi189@gmail.com <tranhoangkhoi189@gmail.com>
2023-02-15 11:17:58 +0700

    Push

--- src/modules/Predict/View/PredictViewManager.tsx
+++ src/modules/Predict/View/PredictViewManager.tsx
@@ -65,7 +65,6 @@ const PredictHistoryViewManager: React.FC = () => {
   const [selectedDay, setSelectedDay] = useState<any>(day[1]);
   //data table
   const [expectedData, setExpectedData] = useState<any>({});
-  
 
   //Open Modal
   const [openModal, setOpenModal] = useState(false);
@@ -107,142 +106,159 @@ const PredictHistoryViewManager: React.FC = () => {
 
   useEffect(() => {
     console.log(expectedData);
-    
   }, [expectedData]);
-  
-  
+
   return (
     <Spin spinning={loading}>
       <div
         className="rounded-xl sm:p-8 p-2 h-full"
         style={{
-      //     background: `linear-gradient(323deg,  ${user.get_customer?.zodiac.color_web_first}
-      // 0%, ${user.get_customer?.zodiac.color_web_second} 99%)`,
-      // background: `${user.get_customer?.zodiac.color_web_first}`,
+          //     background: `linear-gradient(323deg,  ${user.get_customer?.zodiac.color_web_first}
+          // 0%, ${user.get_customer?.zodiac.color_web_second} 99%)`,
+          // background: `${user.get_customer?.zodiac.color_web_first}`,
           backgroundImage: `url("https://images.unsplash.com/photo-1464802686167-b939a6910659?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1150&q=80")`,
-          backgroundSize: 'cover',
-          position: 'relative',
-          overflow: 'hidden',
-          backgroundRepeat: 'no-repeat',
+          backgroundSize: "cover",
+          position: "relative",
+          overflow: "hidden",
+          backgroundRepeat: "no-repeat",
         }}
       >
         <div
-        style={{
-          position: 'absolute',
-          background: `${user.get_customer?.zodiac.color_web_first}`,
-          opacity: 0.85,
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
+          style={{
+            position: "absolute",
+            background: `${user.get_customer?.zodiac.color_web_first}`,
+            opacity: 0.85,
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+        ></div>
+        <div
+          style={{
+            position: "relative",
+            zIndex: 5,
+          }}
+        >
+          <div
+            className=" text-3xl font-poppins py-5 font-bold text-center uppercase "
+            style={{ color: `${user.get_customer?.zodiac.color_web_second}` }}
+          >
+            Dự báo theo ngày
+          </div>
 
-        }}></div>
-       <div
-       style={{
-        position: 'relative',
-        zIndex: 5,
-       }}>
-        <div className=" text-3xl font-poppins py-5 font-bold text-center uppercase "
-        style={{color: `${user.get_customer?.zodiac.color_web_second}` }}>
-          Dự báo theo ngày
-        </div>
-
-        <div className="justify-center flex items-center h-16 mb-5 border-4">
-          <div className="lg:col-span-4 hidden sm:block"></div>
-          {day.map((item) => (
-            <button
-              onClick={() => {
-                setSelectedDay(item);
-              }}
-              key={item.id}
-              className="bg-transparent border-none lg:col-span-1 md:col-span-3 col-span-1 w-auto mx-3"
-            >
-              <div
-                className={
-                  item.id == selectedDay.id
-                    ? "border-active " 
-                    : "border1"
-                }
+          <div className="justify-center flex items-center h-16 mb-5 border-4">
+            <div className="lg:col-span-4 hidden sm:block"></div>
+            {day.map((item) => (
+              <button
+                onClick={() => {
+                  setSelectedDay(item);
+                }}
+                key={item.id}
+                className="bg-transparent border-none lg:col-span-1 md:col-span-3 col-span-1 w-auto mx-3"
               >
-                {item.name}
-              </div>
-            </button>
-          ))}
-        </div>
-
-        <div className="sm:hidden justify-center flex flex-row">
-          <img
-            src={`../../../../public/image/zodiac/12.png`}
-           
-            className="w-2/3 invert "
-          />
-        </div>
-
-        <div className="grid sm:grid-cols-3 grid-cols-2 sm:gap-0 gap-4 grid-rows-2 place-items-center h-full">
-          <div className="first sm:text-2xl text-md font-semibold text-OnPrimaryLight">
-            <div>
-              {""} Tình cảm {""}
-            </div>
-            <Rate
-              disabled
-              value={expectedData?.love}
-              className="sm:text-3xl text-lg mt-3  stroke-[40] "
-              style={{stroke:  `${user.get_customer?.zodiac.color_web_second}` }}
-            />
+                <div
+                  style={
+                    item.id == selectedDay.id
+                      ? {
+                          paddingBottom: "8px",
+                          borderBottom: `2px solid ${user.get_customer?.zodiac.color_web_second}`,
+                        }
+                      : { border: "none", paddingBottom: "8px" }
+                  }
+                  className="text-white text-xl font-bold"
+                >
+                  {item.name}
+                </div>
+              </button>
+            ))}
           </div>
 
-          <div className="sm:block hidden row-span-2 text-center w-full justify-center">
+          <div className="sm:hidden justify-center flex flex-row">
             <img
-              src={`../../../image/zodiac/3.png`}
-              className=" " //invert
-              style={{marginTop: 10, paddingBottom: 10, width: 450, height: 320, marginLeft: -27 }}
-
-            />
-          </div>
-          <div className="second sm:text-2xl text-md font-semibold text-OnPrimaryLight">
-            <div>
-              {""} Công việc {""}
-            </div>
-            <Rate
-              disabled
-              value={expectedData?.job}
-              className="sm:text-3xl text-lg mt-3  stroke-[40] "
-              style={{stroke:  `${user.get_customer?.zodiac.color_web_second}` }}
+              src={`../../../../public/image/zodiac/${listPredict.data?.zodiac_id}.png`}
+              className="w-2/3 invert "
             />
           </div>
 
-          <div className="third sm:text-2xl text-md font-semibold text-OnPrimaryLight">
-            <div>
-              {""} Sức khỏe {""}
+          <div className="grid sm:grid-cols-3 grid-cols-2 sm:gap-0 gap-4 grid-rows-2 place-items-center h-full">
+            <div className="first sm:text-2xl text-md font-semibold text-OnPrimaryLight">
+              <div>
+                {""} Tình cảm {""}
+              </div>
+              <Rate
+                disabled
+                value={expectedData?.love}
+                className="sm:text-3xl text-lg mt-3  stroke-[40] "
+                style={{
+                  stroke: `${user.get_customer?.zodiac.color_web_second}`,
+                }}
+              />
             </div>
-            <Rate
-              disabled
-              value={expectedData?.health}
-              className="sm:text-3xl text-lg mt-3  stroke-[40] "
-              style={{stroke:  `${user.get_customer?.zodiac.color_web_second}` }}
-            />
-          </div>
-          <div className="fourth sm:text-2xl text-md font-semibold text-OnPrimaryLight">
-            <div>
-              {""} Tài chính {""}
+
+            <div className="sm:block hidden row-span-2 text-center w-full justify-center">
+              <img
+                src={`../../../image/zodiac/${listPredict.data?.zodiac_id}.png`}
+                className=" " //invert
+                style={{
+                  marginTop: 10,
+                  paddingBottom: 10,
+                  width: 450,
+                  height: 320,
+                  marginLeft: -27,
+                }}
+              />
             </div>
-            <Rate
-              disabled
-              value={expectedData?.money}
-              className="sm:text-3xl text-lg mt-3 stroke-[40] "
-              style={{stroke:  `${user.get_customer?.zodiac.color_web_second}` }}
-            />
+            <div className="second sm:text-2xl text-md font-semibold text-OnPrimaryLight">
+              <div>
+                {""} Công việc {""}
+              </div>
+              <Rate
+                disabled
+                value={expectedData?.job}
+                className="sm:text-3xl text-lg mt-3  stroke-[40] "
+                style={{
+                  stroke: `${user.get_customer?.zodiac.color_web_second}`,
+                }}
+              />
+            </div>
+
+            <div className="third sm:text-2xl text-md font-semibold text-OnPrimaryLight">
+              <div>
+                {""} Sức khỏe {""}
+              </div>
+              <Rate
+                disabled
+                value={expectedData?.health}
+                className="sm:text-3xl text-lg mt-3  stroke-[40] "
+                style={{
+                  stroke: `${user.get_customer?.zodiac.color_web_second}`,
+                }}
+              />
+            </div>
+            <div className="fourth sm:text-2xl text-md font-semibold text-OnPrimaryLight">
+              <div>
+                {""} Tài chính {""}
+              </div>
+              <Rate
+                disabled
+                value={expectedData?.money}
+                className="sm:text-3xl text-lg mt-3 stroke-[40] "
+                style={{
+                  stroke: `${user.get_customer?.zodiac.color_web_second}`,
+                }}
+              />
+            </div>
           </div>
-        </div>
-        {/* <div
+          {/* <div
           className="text-white text-xl font-poppins font-semibold text-center pt-5 uppercase
                 color: #fcba03
                 "
         >
           Ngày sinh: {listPredict.data?.birthday}
         </div> */}
-        <div className="w-full h-full bg-no-repeat bg-cover pt-3">
-          {/* <Text
+          <div className="w-full h-full bg-no-repeat bg-cover pt-3">
+            {/* <Text
             style={{
               color: "#fff",
               textShadow: "0px 0px 10px #fcba03",
@@ -252,70 +268,105 @@ const PredictHistoryViewManager: React.FC = () => {
               {listPredict.data?.zodiac.name}
             </h1>
           </Text> */}
-          <div className="grid grid-cols-6 auto-rows-max gap-2 justify-items-center">
-            <div className="rounded-2xl md:col-span-2 col-span-3 border-slate-400 h-full w-full grid  bg-opacity-10 text-white shadow-gray-400"
-            style={{background: `${user.get_customer?.zodiac.color_web_first}`,
-            opacity: 0.85}}>
-              <div className="px-5 py-3  self-center text-[18px] uppercase  font-bold"
-               style={{color: `${user.get_customer?.zodiac.color_web_second}`,
-              }}>
-                Màu may mắn
+            <div className="grid grid-cols-6 auto-rows-max gap-2 justify-items-center">
+              <div
+                className="rounded-2xl md:col-span-2 col-span-3 border-slate-400 h-full w-full grid  bg-opacity-10 text-white shadow-gray-400"
+                style={{
+                  background: `${user.get_customer?.zodiac.color_web_first}`,
+                  opacity: 0.85,
+                }}
+              >
+                <div
+                  className="px-5 py-3  self-center text-[18px] uppercase  font-bold"
+                  style={{
+                    color: `${user.get_customer?.zodiac.color_web_second}`,
+                  }}
+                >
+                  Màu may mắn
+                </div>
+                <p className="justify-self-center self-start text-center">
+                  {expectedData?.color_lucky}
+                </p>
               </div>
-              <p className="justify-self-center self-start text-center">
-                {expectedData?.color_lucky}
-              </p>
-            </div>
-            <div className="rounded-2xl md:col-span-2 col-span-3 border-slate-400 h-full w-full grid bg-opacity-10 text-white"
-             style={{background: `${user.get_customer?.zodiac.color_web_first}`,
-             opacity: 0.85}}>
-              <div className="px-5 py-3  self-center text-[18px] uppercase font-bold"
-              style={{color: `${user.get_customer?.zodiac.color_web_second}`,
-            }}>
-                Số may mắn
+              <div
+                className="rounded-2xl md:col-span-2 col-span-3 border-slate-400 h-full w-full grid bg-opacity-10 text-white"
+                style={{
+                  background: `${user.get_customer?.zodiac.color_web_first}`,
+                  opacity: 0.85,
+                }}
+              >
+                <div
+                  className="px-5 py-3  self-center text-[18px] uppercase font-bold"
+                  style={{
+                    color: `${user.get_customer?.zodiac.color_web_second}`,
+                  }}
+                >
+                  Số may mắn
+                </div>
+                <p className="justify-self-center self-start text-center">
+                  {expectedData?.number_lucky}
+                </p>
               </div>
-              <p className="justify-self-center self-start text-center">
-                {expectedData?.number_lucky}
-              </p>
-            </div>
-            <div className="rounded-2xl md:col-span-2 col-span-6 border-slate-400 h-full w-full grid  bg-opacity-10 text-white"
-             style={{background: `${user.get_customer?.zodiac.color_web_first}`,
-             opacity: 0.85}}>
-              <div className="px-5 py-3 self-center text-[18px] uppercase font-bold"
-              style={{color: `${user.get_customer?.zodiac.color_web_second}`,
-            }}>
-                Cung hoàng đạo may mắn
+              <div
+                className="rounded-2xl md:col-span-2 col-span-6 border-slate-400 h-full w-full grid  bg-opacity-10 text-white"
+                style={{
+                  background: `${user.get_customer?.zodiac.color_web_first}`,
+                  opacity: 0.85,
+                }}
+              >
+                <div
+                  className="px-5 py-3 self-center text-[18px] uppercase font-bold"
+                  style={{
+                    color: `${user.get_customer?.zodiac.color_web_second}`,
+                  }}
+                >
+                  Cung hoàng đạo may mắn
+                </div>
+                <p className="justify-self-center self-start text-center">
+                  {expectedData?.zodiac_lucky}
+                </p>
               </div>
-              <p className="justify-self-center self-start text-center">
-                {expectedData?.zodiac_lucky}
-              </p>
-            </div>
-            <div className="rounded-2xl col-span-6 h-full w-full grid border-slate-400  bg-opacity-10 text-white"
-             style={{background: `${user.get_customer?.zodiac.color_web_first}`,
-             opacity: 0.85}}>
-              <div className="px-5 py-3  self-center text-[18px] uppercase  font-bold"
-              style={{color: `${user.get_customer?.zodiac.color_web_second}`,
-            }}>
-                Dự báo trong ngày
+              <div
+                className="rounded-2xl col-span-6 h-full w-full grid border-slate-400  bg-opacity-10 text-white"
+                style={{
+                  background: `${user.get_customer?.zodiac.color_web_first}`,
+                  opacity: 0.85,
+                }}
+              >
+                <div
+                  className="px-5 py-3  self-center text-[18px] uppercase  font-bold"
+                  style={{
+                    color: `${user.get_customer?.zodiac.color_web_second}`,
+                  }}
+                >
+                  Dự báo trong ngày
+                </div>
+                <p className="justify-self-center self-start text-justify px-7">
+                  {expectedData?.expected}
+                </p>
               </div>
-              <p className="justify-self-center self-start text-justify px-7">
-                {expectedData?.expected}
-              </p>
-            </div>
-            <div className="rounded-2xl col-span-6  h-full w-full grid border-slate-400  bg-opacity-10 text-white "
-             style={{background: `${user.get_customer?.zodiac.color_web_first}`,
-             opacity: 0.85}}>
-              <div className="px-5 py-3 self-center text-[18px] uppercase font-bold"
-              style={{color: `${user.get_customer?.zodiac.color_web_second}`,
-            }}>
-                Lời khuyên hôm nay
+              <div
+                className="rounded-2xl col-span-6  h-full w-full grid border-slate-400  bg-opacity-10 text-white "
+                style={{
+                  background: `${user.get_customer?.zodiac.color_web_first}`,
+                  opacity: 0.85,
+                }}
+              >
+                <div
+                  className="px-5 py-3 self-center text-[18px] uppercase font-bold"
+                  style={{
+                    color: `${user.get_customer?.zodiac.color_web_second}`,
+                  }}
+                >
+                  Lời khuyên hôm nay
+                </div>
+                <p className="justify-self-center self-start text-justify px-7">
+                  {expectedData?.advice}
+                </p>
               </div>
-              <p className="justify-self-center self-start text-justify px-7">
-                {expectedData?.advice}
-              </p>
             </div>
           </div>
         </div>
-        </div> 
       </div>
     </Spin>
   );
--- src/modules/Predict/View/ZodiacViewManager.tsx
+++ src/modules/Predict/View/ZodiacViewManager.tsx
@@ -13,8 +13,6 @@ const ZodiacViewManager: React.FC = () => {
   //useAppSelect
   const user = useAppSelector((state) => state.rootReducer.auth.user);
   const listPredict = useAppSelector((state) => state.predict.listPredict);
-  console.log("user", user);
-
   //state view
 
   //Open Modal
@@ -40,81 +38,110 @@ const ZodiacViewManager: React.FC = () => {
     <div
       className="container relative mx-auto items-center "
       style={{
-        background: `linear-gradient(323deg,  ${listPredict.data?.zodiac.color_web_first}
-        0%, ${listPredict.data?.zodiac.color_web_second} 99%)`,
+        backgroundColor: user.get_customer?.zodiac.color_web_first,
       }}
     >
       <div className="w-full h-full absolute grid place-items-center auto-cols-fr auto-rows-fr">
         <img
-          className="w-1/2 invert blur-[0px] opacity-[15%]"
+          // className="w-1/2 invert blur-[0px] opacity-[15%]"
+          className="w-full blur-[0px] opacity-[15%]"
           alt="hero"
           src={`../../../image/zodiac/${listPredict.data?.zodiac_id}.png`}
         />
       </div>
       <div
-        className="z-10 w-full lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center  shadow-2xl  p-10
-      bg-white bg-opacity-10
+        className="z-10 w-full lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center  shadow-2xl  p-10 bg-opacity-10
       "
       >
         {" "}
-        <Text
+        <div
           style={{
             color: "#fff",
-            textShadow: "0px 0px 10px #fcba03",
+            textShadow: `0px 0px 10px ${user.get_customer?.zodiac.color_web_second}`,
           }}
+          className="w-full flex items-center justify-center flex-col"
         >
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-Libra">
-            {listPredict.data?.zodiac.name} /{" "}
-            {listPredict.data?.zodiac.name_second} /
-            {listPredict.data?.zodiac.codeName}
+          <h1
+            style={{ color: user.get_customer?.zodiac.color_web_second }}
+            className="title-font sm:text-4xl text-3xl mb-4 font-bold uppercase"
+          >
+            Cung {listPredict.data?.zodiac.name}
           </h1>
-        </Text>
-        <p className="mb-8 leading-relaxed text-white">
-          Màu sắc: {listPredict.data?.zodiac.favColorName}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Biểu tượng: {listPredict.data?.zodiac.icon}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Hành tinh: {listPredict.data?.zodiac.star_of_destiny}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Màu may mắn: {listPredict.data?.zodiac.color_lucky}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Đá quí: {listPredict.data?.zodiac.gemVN}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Thần bảo hộ: {listPredict.data?.zodiac.protector}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Hợp mệnh nam: {listPredict.data?.zodiac.suitableMale}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Hợp mệnh nữ: {listPredict.data?.zodiac.suitableFemale}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Tổng quan: {listPredict.data?.zodiac.general_features}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Điểm mạnh: {listPredict.data?.zodiac.strengths_in_character}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Điểm yếu: {listPredict.data?.zodiac.weakness_in_character}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Cá tính nam : {listPredict.data?.zodiac.personalityMale}, Cá tính nữ :{" "}
-          {listPredict.data?.zodiac.personalityFemale}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Công việc : {listPredict.data?.zodiac.career}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Sức khỏe : {listPredict.data?.zodiac.health}
-        </p>
-        <p className="mb-8 leading-relaxed text-white">
-          Sự đồng cảm : {listPredict.data?.zodiac.affection}
-        </p>
+          <p className="text-lg my-2 capitalize">
+            {listPredict.data?.zodiac.name_second} -{" "}
+            {listPredict.data?.zodiac.codeName}
+          </p>
+        </div>
+        {/* Content */}
+        <div>
+          <div className="pt-6 flex items-center justify-between">
+            {/*  */}
+            <div className="bg-before-white py-2 px-6 rounded-xl overflow-hidden">
+              <p className="leading-relaxed text-white">
+                Màu sắc: {listPredict.data?.zodiac.favColorName}
+              </p>
+              <p className="leading-relaxed text-white">
+                Biểu tượng: {listPredict.data?.zodiac.icon}
+              </p>
+              <p className="leading-relaxed text-white">
+                Hành tinh: {listPredict.data?.zodiac.star_of_destiny}
+              </p>
+            </div>
+
+            {/*  */}
+            <div className="bg-before-white py-2 px-6 rounded-xl overflow-hidden">
+              <p className=" leading-relaxed text-white">
+                Màu may mắn: {listPredict.data?.zodiac.color_lucky}
+              </p>
+              <p className=" leading-relaxed text-white">
+                Đá quí: {listPredict.data?.zodiac.gemVN}
+              </p>
+              <p className=" leading-relaxed text-white">
+                Thần bảo hộ: {listPredict.data?.zodiac.protector}
+              </p>
+            </div>
+
+            {/*  */}
+            <div className="bg-before-white py-2 px-6 rounded-xl overflow-hidden">
+              <div>
+                <p className=" leading-relaxed text-white">
+                  Hợp mệnh nam: {listPredict.data?.zodiac.suitableMale}
+                </p>
+                <p className=" leading-relaxed text-white">
+                  Hợp mệnh nữ: {listPredict.data?.zodiac.suitableFemale}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/*  */}
+          <div>
+            <p className="mb-8 leading-relaxed text-white">
+              Tổng quan: {listPredict.data?.zodiac.general_features}
+            </p>
+          </div>
+
+          <p className="mb-8 leading-relaxed text-white">
+            Điểm mạnh: {listPredict.data?.zodiac.strengths_in_character}
+          </p>
+          <p className="mb-8 leading-relaxed text-white">
+            Điểm yếu: {listPredict.data?.zodiac.weakness_in_character}
+          </p>
+          <p className="mb-8 leading-relaxed text-white">
+            Cá tính nam : {listPredict.data?.zodiac.personalityMale}, Cá tính nữ
+            : {listPredict.data?.zodiac.personalityFemale}
+          </p>
+          <p className="mb-8 leading-relaxed text-white">
+            Công việc : {listPredict.data?.zodiac.career}
+          </p>
+          <p className="mb-8 leading-relaxed text-white">
+            Sức khỏe : {listPredict.data?.zodiac.health}
+          </p>
+          <p className="mb-8 leading-relaxed text-white">
+            Sự đồng cảm : {listPredict.data?.zodiac.affection}
+          </p>
+        </div>
+        {/*  */}
       </div>
     </div>
   );
